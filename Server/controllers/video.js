cb30c0b952602e0750762565af518de42ffb29b8
Bilal Javaid <sb100513@gmail.com>
2023-12-06 12:53:06 +0500

    bug

--- Server/controllers/video.js
+++ Server/controllers/video.js
@@ -71,7 +71,7 @@ export const addView = async (req,res,next)=>{
 }
 export const random = async (req,res,next)=>{
     try {
-        const videos  = await Video.aggregate([{$sample:{size: 3}}]);
+        const videos  = await Video.aggregate([{$sample:{size: 4}}]);
         res.status(200).json(videos)
     } catch (error) {
         next(error);
